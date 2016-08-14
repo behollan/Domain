5229e3304f185eaf0d0aaa1122b7cd384ae9418b
Ben Holland <Benjamin.holland1@gmail.com>
2016-08-13 22:13:53 -0600

    Initial commit of the InOutBoard as well as the Bluetooth Key File and and a testing python file.

---
+++ inoutboard.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+
+#Import Relavent Libraries
+import curses               #For CL GUI
+import bluetooth            #Communicate over Bluetooth
+import time                 #For getting current time
+import RPi.GPIO as GPIO     #Get GPIO pins for Pins
+
+
+GPIO.setmode(GPIO.BOARD)    #Set GPIO Pins to board
+
+
+#Import Device names and hardware IDs
+keyDat = open('keyDat.txt','r')     #Open the device file in read only mode
+devices = {}                        #Place holder dictionary for device names and hardware IDs (keys and values)
+
+for line in keyDat:                 #The following for loop sets name:ID pairs in the devices dict
+        (key, val)= line.split()
+        devices[key]= val
+
+
+keyDat.close()                      #Close keydat file
+
+names = list(devices.keys())        #Create an ordered list of names
+ids = list(devices.values())        #Create an ordered list of IDs 
+
+
+#CL GUI definition
+def main(stdscr):
+    curses.init_pair(1,curses.COLOR_RED,curses.COLOR_BLACK)
+    curses.init_pair(2,curses.COLOR_GREEN,curses.COLOR_BLACK)
+    while True:
+    # Clear screen
+        stdscr.clear()
+
+        NumDevices=len(names)
+        stdscr.addstr(1,1,str("In/Out Board"))
+        stdscr.addstr(2,1,str("Last checked on " + time.strftime("%a, %d %b %Y %H:%M:%S local time.", time.localtime())))
+        for jj in range (0,NumDevices):
+            result = bluetooth.lookup_name(ids[jj], timeout=5)
+            if (result != None):
+                stdscr.addstr(jj+3,1,str("%s is: in" %names[jj]),curses.color_pair(2))
+            else:
+                stdscr.addstr(jj+3,1,str("%s is: out" %names[jj]),curses.color_pair(1))
+        stdscr.refresh()
+        stdscr.getkey()
+#        time.sleep(30)
+curses.wrapper(main)
+'''
+while True:
+        NumDevices=len(names)
+        print("In/Out Board")
+        print("Last checked on " + time.strftime("%a, %d %b %Y %H:%M:%S local time.", time.localtime()))
+        for jj in range (0,NumDevices):
+            result = bluetooth.lookup_name(ids[jj], timeout=5)
+            if (result != None):
+                print("%s is: in" %(names[jj]))
+            else:
+                print("%s is: out" %(names[jj]))
+'''
